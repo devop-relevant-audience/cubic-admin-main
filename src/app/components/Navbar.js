9779d30e48f0706626ff0f2fca6dfc91b3efff52
devop-relevant-audience <devop@relevantaudience.com>
2026-06-29 17:46:31 +0700

    Changed logo.

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -1,10 +1,10 @@
-import Image from "next/image";
-import { usePathname, useRouter } from "next/navigation";
-import { cn } from "@/lib/utils";
-import { userStore } from "../store/user_store";
-import { CubicLogo, HamburgerIcon, LogoutIcon } from "../myicon/myicon";
-import Sidebar from "./Sidebar";
-import { useState } from "react";
+import Image from 'next/image';
+import { usePathname, useRouter } from 'next/navigation';
+import { cn } from '@/lib/utils';
+import { userStore } from '../store/user_store';
+import { CubicLogo, HamburgerIcon, LogoutIcon } from '../myicon/myicon';
+import Sidebar from './Sidebar';
+import { useState } from 'react';
 
 const Navbar = ({}) => {
   const { logout } = userStore();
@@ -14,35 +14,27 @@ const Navbar = ({}) => {
 
   const submitLogout = () => {
     logout();
-    router.push("/");
+    router.push('/');
   };
   return (
-    <div className="">
-      <div
-        className={cn(
-          " fixed z-10 w-full h-20 pl-[55px] pr-[64px] flex justify-between items-center  bg-[#282828]  ",
-          pathname === "/" ? "hidden" : ""
-        )}
-      >
-        <div
-          className="cursor-pointer"
-          onClick={() => setShowSidebar(!showSidebar)}
-        >
+    <div className=''>
+      <div className={cn(' fixed z-10 w-full h-20 pl-[55px] pr-[64px] flex justify-between items-center  bg-[#282828]  ', pathname === '/' ? 'hidden' : '')}>
+        <div className='cursor-pointer' onClick={() => setShowSidebar(!showSidebar)}>
           <HamburgerIcon />
         </div>
         <div>
-          <CubicLogo />
+          <Image src='/logo.avif' alt='gymx logo' width={200} height={200} />
         </div>
-        <div className="flex items-center space-x-[11px] font-BebasNeue text-lg h-[22px]">
-          <div className="text-cubic-white  ">Admin</div>
-          <div className=" w-[1px] h-full bg-white"></div>
-          <div onClick={submitLogout} className="  text-cubic-white ">
-            {" "}
+        <div className='flex items-center space-x-[11px] font-BebasNeue text-lg h-[22px]'>
+          <div className='text-cubic-white  '>Admin</div>
+          <div className=' w-[1px] h-full bg-white'></div>
+          <div onClick={submitLogout} className='  text-cubic-white '>
+            {' '}
             Logout
           </div>
         </div>
       </div>
-      <div className="">
+      <div className=''>
         <Sidebar setShowSidebar={setShowSidebar} showSidebar={showSidebar} />
       </div>
     </div>
